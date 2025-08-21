e833501ae01f644fd69a2e6032b79727667c8dc8
Illia Achour <ilyaachour@gmail.com>
2025-08-21 11:16:55 -0400

    Extend iOS FirebaseAuthException mapping & handling

--- kmpauth-firebase/src/iosMain/kotlin/com/mmk/kmpauth/firebase/FirebaseExtensions.ios.kt
+++ kmpauth-firebase/src/iosMain/kotlin/com/mmk/kmpauth/firebase/FirebaseExtensions.ios.kt
@@ -1,25 +1,68 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
 package com.mmk.kmpauth.firebase
 
+import cocoapods.FirebaseAuth.FIRAuthErrorDomain
+import cocoapods.FirebaseAuth.FIRAuthErrorUserInfoNameKey
 import com.mmk.kmpauth.core.FirebaseAuthErrorType
 import dev.gitlive.firebase.auth.FirebaseAuthException
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSError
 
 public actual fun getAuthErrorType(e: FirebaseAuthException): FirebaseAuthErrorType {
-    val message = e.message ?: return FirebaseAuthErrorType.UNKNOWN
-    return when {
-        message.contains("User has already been linked to the given provider.") -> {
-            FirebaseAuthErrorType.PROVIDER_ALREADY_LINKED
-        }
+    val nsError = (e.cause as? NSError)
+    return if (nsError != null && nsError.domain == FIRAuthErrorDomain) {
+        val stringErrorCode = nsError.userInfo[FIRAuthErrorUserInfoNameKey] as? String
+        if (stringErrorCode != null) {
+            when (stringErrorCode) {
+                "ERROR_PROVIDER_ALREADY_LINKED" -> {
+                    FirebaseAuthErrorType.PROVIDER_ALREADY_LINKED
+                }
 
-        message.contains("This credential is already associated with a different user account.") -> {
-            FirebaseAuthErrorType.CREDENTIAL_ALREADY_IN_USE
-        }
+                "ERROR_CREDENTIAL_ALREADY_IN_USE" -> {
+                    FirebaseAuthErrorType.CREDENTIAL_ALREADY_IN_USE
+                }
 
-        message.contains("An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.") -> {
-            FirebaseAuthErrorType.ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL
-        }
+                "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> {
+                    FirebaseAuthErrorType.ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL
+                }
 
-        else -> {
-            return FirebaseAuthErrorType.UNKNOWN
+                else -> {
+                    FirebaseAuthErrorType.UNKNOWN
+                }
+            }
+        } else {
+            val errorCode = nsError.code
+            when (errorCode) {
+                17015L -> FirebaseAuthErrorType.PROVIDER_ALREADY_LINKED
+                17025L -> FirebaseAuthErrorType.CREDENTIAL_ALREADY_IN_USE
+                17012L -> FirebaseAuthErrorType.ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL
+                else -> FirebaseAuthErrorType.UNKNOWN
+            }
+        }
+    } else {
+        // Fallback
+        val message = e.message ?: return FirebaseAuthErrorType.UNKNOWN
+        when {
+            message.contains("User has already been linked to the given provider.")
+                    || message.contains("User can only be linked to one identity for the given provider.")
+                    || message.contains("ERROR_PROVIDER_ALREADY_LINKED") -> {
+                FirebaseAuthErrorType.PROVIDER_ALREADY_LINKED
+            }
+
+            message.contains("This credential is already associated with a different user account.")
+                    || message.contains("ERROR_CREDENTIAL_ALREADY_IN_USE") -> {
+                FirebaseAuthErrorType.CREDENTIAL_ALREADY_IN_USE
+            }
+
+            message.contains("An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.")
+                    || message.contains("ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL") -> {
+                FirebaseAuthErrorType.ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL
+            }
+
+            else -> {
+                return FirebaseAuthErrorType.UNKNOWN
+            }
         }
     }
 }
